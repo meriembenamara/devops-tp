6bd3fb5b6b8d7f7d3e34fae9b30622177766164c
Meriem ben amara <meriembenamara001@gmail.com>
2025-03-10 01:32:45 +0100

    Modification de app.py

--- hello-devops/app.py
+++ hello-devops/app.py
@@ -1,1 +1,13 @@
-print("Hello, CI/CD!")
+from flask import Flask
+
+# Créer l'application Flask
+app = Flask(__name__)
+
+# Définir un point de terminaison pour la page d'accueil
+@app.route('/')
+def hello():
+    return "Hello, CI/CD!"
+
+# Lancer l'application sur toutes les interfaces réseau et sur le port 5000
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
